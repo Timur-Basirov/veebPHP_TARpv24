326993d7020ab6205cc1ca7e11a9ec68ff79eee7
Timur Basirov <atimur.basirov@gmail.com>
2025-11-18 09:35:41 +0200

    git käsud on kirjutatud;

--- gitKasutamine.php
+++ gitKasutamine.php
@@ -29,3 +29,20 @@ echo "</li>";
 </li>
 <?php
 echo "<li>GITHUB projektiga sidumine";
+echo "<pre>";
+echo "git remote add origin git@github.com:Timur-Basirov/veebPHP_TARpv24.git
+
+git branch -M main
+
+git push -u origin main";
+echo "</pre>";
+echo "</li>";
+
+echo "<li>Projekti kloonimine desktopi.<br> 
+* Kontrolli et ir_rsa võti on olemas .ssh kaustas <br>
+* GIT CMD on lahti ja <br>";
+echo "<pre>";
+echo "git clone git@github.com:Timur-Basirov/veebPHP_TARpv24.git";
+echo "</pre>";
+echo "</li>";
+echo "/ol>";
